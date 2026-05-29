320b7f0b56169e23c9b9631e62435a8a067f35c2
Evelyn Thai <evelyn.thai09@gmail.com>
2026-05-29 17:25:51 -0400

    finish transition

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/PropsAltar.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/PropsAltar.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//when something get into the alta, make the runes glow
 namespace Cainos.PixelArtTopDown_Basic
 {
-
     public class PropsAltar : MonoBehaviour
     {
         public List<SpriteRenderer> runes;
         public float lerpSpeed;
+
+        public bool IsLit { get; private set; } = false;
 
         private Color curColor;
         private Color targetColor;
@@ -22,21 +22,20 @@ namespace Cainos.PixelArtTopDown_Basic
         private void OnTriggerEnter2D(Collider2D other)
         {
             targetColor.a = 1.0f;
+            IsLit = true;
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
             targetColor.a = 0.0f;
+            IsLit = false;
         }
 
         private void Update()
         {
             curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
-
             foreach (var r in runes)
-            {
                 r.color = curColor;
-            }
         }
     }
 }
