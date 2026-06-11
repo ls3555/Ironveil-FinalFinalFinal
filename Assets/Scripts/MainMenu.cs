430a482d6a55c362e54ce363a0df5bd424664f9c
evely-thai <evelyn.thai09@gmail.com>
2026-06-11 10:38:17 -0400

    update MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,10 +3,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public FadeImg fader;
     public void ClickAndLoad()
     {
-        StartCoroutine(fader.FadeOut());
         Invoke("LoadGame", 0.5f);
     }
     public void LoadGame()
